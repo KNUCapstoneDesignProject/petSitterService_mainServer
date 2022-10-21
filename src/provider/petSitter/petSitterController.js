3481da65f9989d0dfddceeee4c8bff6c5b1dd046
민욱 <alsdnrdl001@gmail.com>
2022-10-21 17:46:45 +0900

    이전 기획에 의한 api 제거

--- src/provider/petSitter/petSitterController.js
+++ src/provider/petSitter/petSitterController.js
@@ -6,20 +6,3 @@ const { response, errResponse } = require("../../../config/response");
 
 const { emit } = require("nodemon");
 
-
-/**
- * API No. 4
- * API Name : 로그인 API
- * [POST] /app/login
- * body : idStr, passsword
- */
-exports.login = async function (req, res) {
-    const { idStr, password } = req.body;
-
-    console.log(password);
-    // TODO: idStr, password 형식적 Validation
-
-    const signInResponse = await petSitterService.postSignIn(idStr, password);
-
-    return res.send(signInResponse);
-};
